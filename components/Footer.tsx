9f14bcd5f4c6561c350b466ed45254a4f9b5af07
arindam234 <aroy92647@gmal.com>
2023-02-12 16:27:09 +0530

    fixed the footer position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full asbolute bottom-0 h-16 lg:h-24 px-4 lg:px-10 bg-[#0e0e21] text-white">
+    <footer className="w-full absolute bottom-0  h-16 lg:h-24 px-4 lg:px-10 bg-[#0e0e21] text-white">
       <div className="relative h-16 md:w-48 w-[100px]  lg:h-24 lg:w-48">
         <Image src={"/images/Header_New.svg"} alt={""} fill />
       </div>
